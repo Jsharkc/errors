5553f205fd2a6996b7fbd56193bdc5bfb330d837
jsharkc <1773867204@qq.com>
2018-08-30 14:04:06 +0800

    设置默认打印栈级别

--- errors.go
+++ errors.go
@@ -7,24 +7,31 @@ import (
 	"strings"
 )
 
+var DefaultStackLevel = 2
+
 type errorWithStack struct {
 	err   error
 	stack string
 }
 
 func (e errorWithStack) Error() string {
-	return fmt.Sprintf("error: %v\nstack: \n%s\n", e.err, e.stack)
+	return fmt.Sprintf("%v\nstack: \n%s\n", e.err, e.stack)
 }
 
 // NewWithLevel create new error with stack level
-func NewWithLevel(err string, level int32) errorWithStack {
+func New(err string) errorWithStack {
+	return NewWithLevel(err, 0)
+}
+
+// NewWithLevel create new error with stack level
+func NewWithLevel(err string, level uint32) errorWithStack {
 	stack := string(debug.Stack())
 	stacks := strings.Split(stack, "\n")
 
 	if level > 0 {
 		stack = strings.Join(stacks[5:5+level*2], "\n")
 	} else {
-		stack = strings.Join(stacks[5:], "\n")
+		stack = strings.Join(stacks[5:5+DefaultStackLevel*2], "\n")
 	}
 
 	er := errorWithStack{
@@ -35,15 +42,20 @@ func NewWithLevel(err string, level int32) errorWithStack {
 	return er
 }
 
+// Wrap wrap error with stack level
+func Wrap(err error) errorWithStack {
+	return WrapWithLevel(err, 0)
+}
+
 // WrapWithLevel wrap error with stack level
-func WrapWithLevel(err error, level int32) errorWithStack {
+func WrapWithLevel(err error, level uint32) errorWithStack {
 	stack := string(debug.Stack())
 	stacks := strings.Split(stack, "\n")
 
 	if level > 0 {
 		stack = strings.Join(stacks[5:5+level*2], "\n")
 	} else {
-		stack = strings.Join(stacks[5:], "\n")
+		stack = strings.Join(stacks[5:5+DefaultStackLevel*2], "\n")
 	}
 
 	er := errorWithStack{
